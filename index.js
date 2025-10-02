f6610cdd0d79fb2a0b735c131bf1506a95e15a85
Adrian Vat <adrianvat24@icloud.com>
2025-10-02 16:36:18 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function loop(){
+    var targetTime = new Date("2025-12-21");
+    var now = new Date();
+    var diff = targetTime - now;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}
+
+setInterval(loop, 1000)
